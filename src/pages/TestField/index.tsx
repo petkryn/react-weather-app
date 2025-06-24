53f4a56393848b3d077688c40baca57dece1b44b
Petro <pkrynytskyi3@gmail.com>
2025-06-24 15:22:36 +0300

    refactor

--- src/pages/TestField/index.tsx
+++ src/pages/TestField/index.tsx
@@ -3,14 +3,10 @@ import Counter from "../../components/Counter";
 import NamesArray from "../../components/NamesArray";
 import TaskList from "../../components/TaskList";
 import { tasks } from "../../data/tasks";
-import City from "../../components/City";
-import { cityInfo } from "../../data/cityInfo";
-import { hello } from "../../data/cityInfo";
 import UserList from "../../components/UserList";
 import { users } from "../../data/users";
 
 const TestField: FC = () => {
-  hello();
   return (
     <div>
       <div>
@@ -19,7 +15,7 @@ const TestField: FC = () => {
 
       <NamesArray />
       <TaskList tasks={tasks} />
-      <City data={cityInfo} />
+
       <UserList users={users} />
     </div>
   );
