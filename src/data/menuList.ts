ae6acfec9464d6369b004426130fa948b3f8c25a
petkryn <140324556+petkryn@users.noreply.github.com>
2025-07-22 17:02:50 +0300

    add menu (#6)

---
+++ src/data/menuList.ts
@@ -0,0 +1,7 @@
+export const menuList = [
+  { name: "Борщ", price: 80 },
+  { name: "Вареники", price: 120 },
+  { name: "Деруни", price: 95 },
+  { name: "Салат", price: 70 },
+  { name: "Картопля варена", price: 78 }
+];
